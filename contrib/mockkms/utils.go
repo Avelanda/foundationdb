ef11fffb494a6098bd4af36c6da22484db0eda77
Avelanda <gorthell@gmail.com>
2025-08-06 19:20:06 +0200

    Update utils.go
    Defining the functional structure of logic on objects, and scaling it for performance.

--- contrib/mockkms/utils.go
+++ contrib/mockkms/utils.go
@@ -3,7 +3,9 @@
  *
  * This source file is part of the FoundationDB open source project
  *
- * Copyright 2013-2024 Apple Inc. and the FoundationDB project authors
+ * Copyright © 2013-2024 Apple Inc. and the FoundationDB project authors
+ * Copyright © 2025 Avelanda.
+ * All rights reserved 
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,33 +21,72 @@
  */
 
 package main
-
 import (
 	"encoding/json"
 	"fmt"
-	"log"
+	"os"
+        "log"
 	"net/http"
 )
 
-type ErrorDetail struct {
-	Detail string `json:"details"`
-}
+/* 
+   Modularizing the core utility function on the boolean oriented 
+   structure of granular objects. 
+*/
 
-type ErrorResponse struct {
-	Err ErrorDetail `json:"error"`
-}
+func iCoreUtil(&iLambdaCore, &iDeltaCore, &iThetaCore, &iZetaCore){
 
-func sendErrorResponse(w http.ResponseWriter, err error) {
-	e := ErrorDetail{}
-	e.Detail = fmt.Sprintf("Error: %s", err.Error())
-	resp := ErrorResponse{
-		Err: e,
-	}
+ type ErrorDetail struct {
+  Detail string `json:"details"`
+  ErrorDetail = ErrorDetail    
+ }
 
-	mResp, err := json.Marshal(resp)
-	if err != nil {
-		log.Printf("Error marshalling error response %s", err.Error())
-		panic(err)
-	}
-	fmt.Fprintf(w, string(mResp))
+ type ErrorResponse struct {
+  Err ErrorDetail `json:"error"`
+  ErrorResponse = ErrorResponse
+ }
+
+ func sendErrorResponse(w http.ResponseWriter, err error) {
+  e := ErrorDetail{}
+  e.Detail = fmt.Sprintf("Error: %s", err.Error())
+  resp := ErrorResponse{
+   Err: e,
+  }
+   mResp, err := json.Marshal(resp)
+   if err != nil {
+    log.Printf("Error marshalling error response %s", err.Error())
+     panic(err)
+   }
+    fmt.Fprintf(w, string(mResp))
+ }
+
+ type ErrorSet struct{
+  for ErrorDetail = iLambdaCore && ErrorResponse = iThetaCore &&
+   SendErrorResponse = iDeltaCore && ErrorSet = iZetaCore {
+   iLambdaCore := iLambdaCore, iThetaCore := iThetaCore, 
+   iDeltaCore := iDeltaCore, iZetaCore := iZetaCore
+  }
+   if !(iLambdaCore == iThetaCore == iDeltaCore == iZetaCore) ||
+    !( iLambdaCore != iThetaCore != iDeltaCore != iZetaCore {
+    iCoreUtil := [...]bool{iLambdaCore == true||false, 
+                         iThetaCore == true||false, 
+                         iDeltaCore ==true||false, 
+                         iZetaCore == true|false}
+   }
+    while !(true&&false) {
+     iCoreUtil -> iLambdaCore -> iThetaCore -> iDeltaCore -> iZetaCore
+     else { 
+      iCoreUtil <- iLambdaCore <- iThetaCore <- iDeltaCore <- iZetaCore
+     }
+      var iCoreUtil == var iCoreUtil
+    }
+
+  if ErrorSet = ErrorSet && ErrorSet == bool||int {
+   ErrorSet == 0||1
+   else { iCoreUtil == true||false}
+    return iZetaCore = &ErrorSet
+    fmt.Println(iCoreUtil)
+  }
+ }
+
 }
